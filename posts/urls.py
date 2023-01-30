63d7a92da95eefadfe89dc9b0b5b4feb7353ddb9
unanchoi <unusualc@likelion.org>
2023-01-30 20:08:29 +0900

    스터디 2회차 자료 업로드

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from posts.views import *
 
 urlpatterns = [
     path('', hello_world, name = 'hello_world'),
+    path('posts/<int:id>/', get_post_detail, name="get_post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-
+from django.views.decorators.http import require_http_methods
+from .models import Post, Comment
 # Create your views here.
 
 def hello_world(request):
@@ -12,3 +13,20 @@ def hello_world(request):
             'message' : '메시지 전달 성공!',
             'data' : "Hello world",
         })
+
+
+@require_http_methods(["GET"])
+def get_post_detail(request, id):
+    post = get_object_or_404(Post, pk = id)
+    category_json={
+                "id"        : post.id,
+                "writer"    : post.writer,
+                "content" : post.content,
+                "category" : post.category,
+    }
+        
+    return JsonResponse({
+                'status': 200,
+                'message': '게시글 조회 성공',
+                'data': category_json
+            })
