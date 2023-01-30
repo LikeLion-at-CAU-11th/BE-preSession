c6fe77db360458fac6e6c5f150a46b475208cefe
unanchoi <unusualc@likelion.org>
2023-01-30 21:53:51 +0900

    2주차 자료 업데이트

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,8 @@ THIRD_PARTY_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
+AUTH_USER_MODEL = 'users.Member'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class Member(AbstractUser):
+    age = models.IntegerField(verbose_name="나이",default=20, null=True)
